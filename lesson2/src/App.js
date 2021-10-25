8172eecb15b7f23da360c0ea1712eb40877c789e
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2021-10-25 17:23:39 +0300

    Попытался использовать React.createRef() для прокрутки к последнему сообщению. Ничего не получается.

--- lesson2/src/App.js
+++ lesson2/src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from "react";
-import {useEffect} from "react";
+import {React, useState, useEffect} from "react";
 import Message from "./Message";
 
 function App() {
@@ -12,7 +11,7 @@ function App() {
 	// ];
 	const [messageList, setMessageList] = useState([]);
 	const [messageId, setMessageId] = useState(0);
-
+	const myRef = React.createRef();
 	useEffect(() => {
 		if ((messageList.length !== 0) && (messageList[messageList.length - 1].author !== 'Bot')) {
 			let message = {id: messageId, author: 'Bot', text: 'Привет от Бота!'};
@@ -21,8 +20,14 @@ function App() {
 				let arr = [...messageList, message];
 				setMessageList(arr);
 			}, 1500);
-
-			console.log(messageList);
+			// if () {
+			// 	.current.scrollIntoView(
+			// 		{
+			// 			behavior: 'smooth',
+			// 			block: 'end',
+			// 			inline: 'nearest'
+			// 		})
+			// }
 		}
 	}, [messageList])
 
@@ -35,27 +40,12 @@ function App() {
 		textInput.value = '';
 		let arr = [...messageList, message];
 		setMessageList(arr);
-		console.log(messageList);
 	};
 
 	return (
 		<div className="App">
-			{/*<header className="App-header">*/}
-			{/*	<img src={logo} className="App-logo" alt="logo"/>*/}
-			{/*	<p>*/}
-			{/*		Edit <code>src/App.js</code> and save to reload.*/}
-			{/*	</p>*/}
-			{/*	<a*/}
-			{/*		className="App-link"*/}
-			{/*		href="https://reactjs.org"*/}
-			{/*		target="_blank"*/}
-			{/*		rel="noopener noreferrer"*/}
-			{/*	>*/}
-			{/*		Learn React*/}
-			{/*	</a>*/}
-			{/*</header>*/}
 			<div className="App-messageList">
-				<Message list={messageList}/>
+				<Message ref={myRef} list={messageList}/>
 			</div>
 
 			<div className="App-forms">
--- lesson2/src/Message.jsx
+++ lesson2/src/Message.jsx
@@ -1,17 +1,19 @@
+import React from "react";
 import "./Message.scss"
 
-const Message = (props) => {
-		return props.list.map((message) => {
-			return (
-				<div className="message" key={message.id}>
-					<div className="message__item">
-						<p>{message.author}</p>
-						<p>{message.text}</p>
-					</div>
+
+const Message = React.forwardRef((props, ref) => {
+	return props.list.map((message) => {
+		return (
+			<div className="message" key={message.id}>
+				<div className="message__item">
+					<p className="message__name">{message.author}:</p>
+					<p>{message.text}</p>
 				</div>
-			)
-		})
-	}
-;
+				<div ref={ref}></div>
+			</div>
+		)
+	})
+});
 
 export default Message;
