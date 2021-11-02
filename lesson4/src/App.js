84dcc29dc674c24fb570387308b2b780eb9336c1
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2021-11-02 16:48:01 +0300

    Заработала маршрутизация страницы чатов приложения по /main

--- lesson4/src/App.js
+++ lesson4/src/App.js
@@ -9,46 +9,20 @@ import { Paper } from "@mui/material";
 import { Header } from "./Components/Header";
 import { Search } from "./Components/Search";
 
+import Main from "./routes/Main";
+
 function App() {
   return (
     <>
       <Switch>
         <Route path="/profile" component={Profile}></Route>
         <Route path="/header" component={Header}></Route>
-        <Route exact path="/home" component={Home}></Route>
+        <Route path="/main" component={Main}></Route>
+        <Route exact path="/" component={Home}></Route>
         <Route>
           <h3>Page not found</h3>
         </Route>
       </Switch>
-      <Paper
-        evaluation={2}
-        variant="outlined"
-        sx={{
-          boxSizing: "border-box",
-          p: "20px 10%",
-          height: "100vh",
-          bgcolor: "background.main",
-        }}
-      >
-        <Paper
-          sx={{
-            bgcolor: "background.second",
-            display: "grid",
-            gridTemplateAreas: `
-              "header header"
-              "chats messages"`,
-            gridTemplateRows: "30px 85vh",
-            gridTemplateColumns: "1fr 3fr",
-            p: 2,
-          }}
-        >
-          <Header />
-          <Chats>
-            <Search />
-          </Chats>
-          <Layout />
-        </Paper>
-      </Paper>
     </>
   );
 }
---
+++ lesson4/src/Components/Main.jsx
@@ -0,0 +1,47 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Home, Profile } from "./routes";
+import "./App.css";
+
+import { Layout } from "./Components/Layout";
+import { Chats } from "./Components/Chats";
+import { Paper } from "@mui/material";
+import { Header } from "./Components/Header";
+import { Search } from "../Components/Search";
+
+function App() {
+  return (
+    <>
+        <Paper
+        evaluation={2}
+        variant="outlined"
+        sx={{
+          boxSizing: "border-box",
+          p: "20px 10%",
+          height: "100vh",
+          bgcolor: "background.main",
+        }}
+      >
+        <Paper
+          sx={{
+            bgcolor: "background.second",
+            display: "grid",
+            gridTemplateAreas: `
+              "header header"
+              "chats messages"`,
+            gridTemplateRows: "30px 85vh",
+            gridTemplateColumns: "1fr 3fr",
+            p: 2,
+          }}
+        >
+          <Header />
+          <Chats>
+            <Search />
+          </Chats>
+          <Layout />
+        </Paper>
+      </Paper>
+    </>
+  );
+}
+
+export default App;
