6a9c49f8cb7a9cba282fa2c8c648c8df03a5b56e
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2021-11-16 18:37:43 +0300

    Сообщения бота добавляются с помощью thuhk.

--- lesson5/src/store/middlewares/addMessageWithThunk.js
+++ lesson5/src/store/middlewares/addMessageWithThunk.js
@@ -1,14 +1,18 @@
 import { MESSAGES_ADD_MESSAGE } from "../messages/action";
 import { addMessageActionCreator } from "../actionCreators/AddMessageActionCreator";
 import { dispatch } from "react-redux";
+import { nanoid } from "nanoid";
 
 export const addMessageWithThunk =
   (chatId, message) => (dispatch, getState) => {
     dispatch(addMessageActionCreator(chatId, message));
     if (message.author !== "Bot") {
       const botMessage = {
-        /* ... */
+        id: nanoid(),
+        author: "Bot",
+        text: "Привет от бота!",
       };
+
       setTimeout(
         () => dispatch(addMessageActionCreator(chatId, botMessage)),
         2000
