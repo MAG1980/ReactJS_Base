f666b9a5b92ae0464961637d864808e0166a7220
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2021-11-17 09:49:20 +0300

    Подключил к приложению Redux-persist. При первой загрузку данные в store подгружаются из LocalStorage.

--- lesson5/src/store/index.js
+++ lesson5/src/store/index.js
@@ -3,14 +3,30 @@ import { createStore, applyMiddleware, compose } from "redux";
 // import { chatsListReducer } from "./chats/reducer";
 import { storeCombineReducer } from "./combineReducers/";
 import thunk from "redux-thunk";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // localStorage
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, storeCombineReducer);
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
-  storeCombineReducer,
-  // profileReducer,
-  // composeEnhancers(addMessageWithThunk(thunk))
+  persistedReducer,
   composeEnhancers(applyMiddleware(thunk))
-
-  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+export const persistor = persistStore(store);
+
+// export const store = createStore(
+//   storeCombineReducer,
+//   // profileReducer,
+//   // composeEnhancers(addMessageWithThunk(thunk))
+//   composeEnhancers(applyMiddleware(thunk))
+
+//   // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
